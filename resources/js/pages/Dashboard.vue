edb53466de481962485a8529e5a032c2feda8829
muhammad jaber <jabirmuhammad126@gmail.com>
2026-08-27 14:30:08 +0500

    fixed ci cd

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -20,7 +20,10 @@ defineProps<{
     <Head title="Dashboard" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
+    <p> welecome to dashboard</p>
+     
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+        
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
